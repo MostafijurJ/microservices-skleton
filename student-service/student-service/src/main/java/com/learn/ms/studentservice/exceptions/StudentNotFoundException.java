5149e3a736d3fd432ac43f703d1b06950da5afac
Mostafijur <mrkajol550@gmail.com>
2023-10-15 00:04:15 +0600

    student service migrate

--- student-service/student-service/src/main/java/com/learn/ms/studentservice/exceptions/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.learn.ms.studentservice.exceptions;
-
-public class StudentNotFoundException extends RuntimeException {
-	public StudentNotFoundException(String message) {
-		super(message);
-	}
-}
--- student-service/student-service/src/main/java/com/learn/ms/studentservice/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learn.ms.studentservice.repository;
-
-import com.learn.ms.studentservice.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-	List<Student> findByName(String name);
-}
